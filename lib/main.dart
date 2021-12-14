3a0e9a1eb25246333d5f5a1310917b03e1df9227
Deepak Kumar <deepakstark23@gmail.com>
2021-12-14 20:58:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:clima/screens/loading_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
+
+//token:- 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
